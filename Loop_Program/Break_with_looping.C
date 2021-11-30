94dad139d47a303cee53297c8a2c50c713d0481d
arifpatel27 <arifpatel.karad@gmail.com>
2021-11-30 10:18:40 +0530

    Loop Program

---
+++ Loop_Program/Break_with_looping.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int i = 0;   //Declaration
+
+    for(i =1; i <= 5; i++)
+    {
+            printf("\n INDIA");
+
+            if(i == 4)
+            {
+                    break;
+            }
+
+            printf("\n I Love INDIA");
+    }
+
+    printf("\n Thanks...");
+
+
+    getche ();
+    return 0;
+}
